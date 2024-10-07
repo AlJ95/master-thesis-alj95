5567094b9da7c9075b659481562784ff42cf2927
Jan Albrecht <ke31umow@studserv.uni-leipzig.de>
2024-10-07 17:32:43 +0200

    progress within indexing, indexing runs successfully without tests

--- code/RAGBench/src/RAGBench/main.py
+++ code/RAGBench/src/RAGBench/main.py
@@ -1,53 +1,8 @@
-import json
+import os, sys
 
-from RAGBench.pipelines.indexing import IngestionPipeline
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
-# Function to load configuration from JSON file
-def load_config(config_file):
-    """
-    Loads configuration from a JSON file.
-
-    Args:
-        config_file (str): Path to the JSON file containing the configuration.
-
-    Returns:
-        dict: The loaded configuration.
-    """
-    with open(config_file, 'r') as file:
-        config = json.load(file)
-    return config
-
-# Base class for pipeline components
-class PipelineComponent:
-    def execute(self, *args, **kwargs):
-        raise NotImplementedError("Subclasses should implement this method.")
-
-# Retrieval Pipeline
-class RetrievalPipeline(PipelineComponent):
-    def __init__(self, index, top_k, embedding_model):
-        self.index = index
-        self.top_k = top_k
-        self.embedding_model = embedding_model
-
-    def execute(self, query: str):
-        raise NotImplementedError("Subclasses should implement this method.")
-    
-# Generation Pipeline
-class GenerationPipeline(PipelineComponent):
-    def __init__(self, api_base_url, api_key, max_tokens, temperature, top_p):
-        pass
-
-    def execute(self, retrieved_docs):
-        raise NotImplementedError("Subclasses should implement this method.")
-    
-
-# Main RAG Pipeline
-class RAGPipeline:
-    def __init__(self, config):
-        self.ingestion_pipeline = None
-        self.retrieval_pipeline = None
-        self.generation_pipeline = None
+from RAGBench.pipelines.indexing import IndexingPipeline, Configuration
 
 
 # Sample usage
@@ -66,7 +21,17 @@ if __name__ == "__main__":
     # response = rag_pipeline.query(query)
     # print(response)
 
-    print("Hello World")
+    print("Starting indexing")
 
-    indexing_pipeline = IngestionPipeline()
+    config = Configuration(
+        chunk_size=512,
+        chunk_overlap=128,
+        embedding_model="intfloat/multilingual-e5-large-instruct",
+        splitting_strategy="sentence",
+    )
+
+    indexing_pipeline = IndexingPipeline(config)
     indexing_pipeline.run()
+
+    print(indexing_pipeline.DATA_DIR)
+    print(indexing_pipeline.LAWS_FILE.exists())
