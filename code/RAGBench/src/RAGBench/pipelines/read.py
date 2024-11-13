8a42bf71fd83249644eea1c0bbcc06510f6df957
Jan Albrecht <ke31umow@studserv.uni-leipzig.de>
2024-11-13 12:00:14 +0100

    fresh new start for implementation with Haystack: - removed previous implementations - start to test implementation of various RAG variants to ensure it is doable with haystack / fastrag -> Naive RAG, SE4AI project with hybrid search is doable

--- code/RAGBench/src/RAGBench/pipelines/read.py
+++
@@ -1,5 +0,0 @@
-"""
-This module represents the "READ" module from the modular architecture LLM 4 RAG Systems: A Survey
-In naive & advanced RAG systems this module would be named "Retrieval Pipeline"
-"""
-
