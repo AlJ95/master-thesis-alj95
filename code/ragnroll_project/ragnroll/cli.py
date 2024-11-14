7e64dc93378c9947fdff95ea17f5c0f0e8296547
Jan Albrecht <ke31umow@studserv.uni-leipzig.de>
2024-11-14 18:09:52 +0100

    refactoring and added CLI support

---
+++ code/ragnroll_project/ragnroll/cli.py
@@ -0,0 +1,43 @@
+"""This module provides the RP To-Do CLI."""
+# rptodo/cli.py
+
+from typing import Optional, List
+
+import typer
+
+from ragnroll import __app_name__, __version__
+
+app = typer.Typer()
+
+@app.command()
+def split_data(
+    path: List[str] = typer.Argument(...),
+):
+    """
+    Process to split data manually
+    """
+
+@app.command()
+def run_evaluations(
+    configuration_file: str = typer.Argument(...),
+    output_directory: str = typer.Argument(...),
+):
+    pass
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{__app_name__} v{__version__}")
+        raise typer.Exit()
+
+@app.callback()
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the application's version and exit.",
+        callback=_version_callback,
+        is_eager=True,
+    )
+) -> None:
+    return
