1a927bdac24c15c396a5975096c8482058c36d3e
root <ke31umow@studserv.uni-leipzig.de>
2025-04-15 05:10:42 +0000

    run-eval.sh

---
+++ code/ragnroll_project/run-eval.sh
@@ -0,0 +1,2 @@
+python -m ragnroll run-evaluations ./configs/configuration_validation/configuration_phase/rag_dense_retriever.yaml ./data/processed/config_val/evaluation_data.json ./data/processed/config_val/corpus ./rag_dense_retriever.csv --no-baselines --experiment-name=config-validation
+python -m ragnroll run-evaluations ./configs/configuration_validation/configuration_phase/rag_sparse_retriever.yaml ./data/processed/config_val/evaluation_data.json ./data/processed/config_val/corpus ./rag_sparse_retriever.csv --no-baselines --experiment-name=config-validation
