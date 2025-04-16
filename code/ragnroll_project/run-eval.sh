a5127efa3c741185089e1f1f1a77a352d1759278
Jan Albrecht <ke31umow@studserv.uni-leipzig.de>
2025-04-16 19:36:23 +0200

    Update evaluation scripts and configuration files for consistency and clarity
    - Modified run-eval.sh to change the experiment name variable from 'configuration_validation' to 'config-validation' for improved clarity.
    - Updated model specifications in rag_dense_retriever.yaml and rag_sparse_retriever.yaml to use a single model entry instead of an array, simplifying the configuration.
    - Ensured consistency across configuration files for better maintainability.

--- code/ragnroll_project/run-eval.sh
+++ code/ragnroll_project/run-eval.sh
@@ -1,9 +1,8 @@
-# environment variables                                                                                                                                     # derived from python -m ragnroll run-evaluations ./configs/baselines/llm_config.yaml ./data/processed/config_val/evaluation_data.json ./data/processed/config_val/corpus ./output.csv --no-baselines --test-size=30
 export config_path=./configs/configuration_validation/configuration_phase/rag_dense_retriever.yaml
 export evaluation_data_path=./data/processed/config_val/evaluation_data.json
 export corpus_path=./data/processed/config_val/corpus
 export output_path=./dense.csv
 export test_size=30
-export experiment_name=configuration_validation
+export experiment_name=config-validation
 
 screen -L -Logfile "${experiment_name}.log" -dmS $experiment_name /srv/master-thesis-alj95/code/ragnroll_project/.venv/bin/python -m ragnroll run-evaluations $config_path $evaluation_data_path $corpus_path $output_path --no-baselines --test-size=$test_size --experiment-name=$experiment_name
