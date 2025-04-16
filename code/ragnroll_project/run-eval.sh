9720b2e7ff03d675763a493bae20cc3354059aa4
root <ke31umow@studserv.uni-leipzig.de>
2025-04-16 14:23:36 +0000

    remove deepseek temporarly

--- code/ragnroll_project/run-eval.sh
+++ code/ragnroll_project/run-eval.sh
@@ -1,2 +1,4 @@
-python -m ragnroll run-evaluations ./configs/configuration_validation/configuration_phase/rag_dense_retriever.yaml ./data/processed/config_val/evaluation_data.json ./data/processed/config_val/corpus ./rag_dense_retriever.csv --no-baselines --experiment-name=config-validation
+python -m ragnroll run-evaluations ./configs/configuration_validation/configuration_phase/rag_dense_retriever.yaml ./data/processed/config_val/evaluation_data.json ./data/processed/config_val/corpus ./rag_dense_retriever.csv --no-baselines --experiment-name=config-validation &
+pid1=$!
+wait $pid1
 python -m ragnroll run-evaluations ./configs/configuration_validation/configuration_phase/rag_sparse_retriever.yaml ./data/processed/config_val/evaluation_data.json ./data/processed/config_val/corpus ./rag_sparse_retriever.csv --no-baselines --experiment-name=config-validation
