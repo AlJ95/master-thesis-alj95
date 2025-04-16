483e85488df55d13f804dfe9a04ee9cb496b09df
Jan Albrecht <ke31umow@studserv.uni-leipzig.de>
2025-04-16 09:45:57 +0200

    Refactor run-eval.sh to use environment variables for configuration
    - Introduced environment variables for configuration paths and parameters to enhance script flexibility.
    - Updated the script to run evaluations in a detached screen session using the defined variables.

--- code/ragnroll_project/run-eval.sh
+++ code/ragnroll_project/run-eval.sh
@@ -1,2 +1,9 @@
-python -m ragnroll run-evaluations ./configs/configuration_validation/configuration_phase/rag_dense_retriever.yaml ./data/processed/config_val/evaluation_data.json ./data/processed/config_val/corpus ./rag_dense_retriever.csv --no-baselines --experiment-name=config-validation
-python -m ragnroll run-evaluations ./configs/configuration_validation/configuration_phase/rag_sparse_retriever.yaml ./data/processed/config_val/evaluation_data.json ./data/processed/config_val/corpus ./rag_sparse_retriever.csv --no-baselines --experiment-name=config-validation
+# environment variables                                                                                                                                     # derived from python -m ragnroll run-evaluations ./configs/baselines/llm_config.yaml ./data/processed/config_val/evaluation_data.json ./data/processed/config_val/corpus ./output.csv --no-baselines --test-size=30
+export config_path=./configs/configuration_validation/configuration_phase/rag_dense_retriever.yaml
+export evaluation_data_path=./data/processed/config_val/evaluation_data.json
+export corpus_path=./data/processed/config_val/corpus
+export output_path=./dense.csv
+export test_size=30
+export experiment_name=configuration_validation
+
+screen -dmS $experiment_name /srv/master-thesis-alj95/code/ragnroll_project/.venv/bin/python -m ragnroll run-evaluations $config_path $evaluation_data_path $corpus_path $output_path --no-baselines --test-size=$test_size
