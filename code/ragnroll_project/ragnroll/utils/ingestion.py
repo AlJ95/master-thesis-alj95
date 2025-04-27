499cea677b2c0714340d91a4bdc49a9a98cd4f61
Jan Albrecht <ke31umow@studserv.uni-leipzig.de>
2025-04-27 15:10:32 +0200

    Enhance SentenceTransformersTextEmbedder initialization: support device configuration and environment variable token handling

--- code/ragnroll_project/ragnroll/utils/ingestion.py
+++ code/ragnroll_project/ragnroll/utils/ingestion.py
@@ -10,6 +10,8 @@ from haystack.components.embedders import (
     HuggingFaceAPIDocumentEmbedder,
     AzureOpenAIDocumentEmbedder
     )
+from haystack.utils.device import ComponentDevice
+from haystack.utils import Secret
 import json
 import logging
 import yaml
@@ -155,6 +157,12 @@ def _get_document_embedder_from_text_embedder(text_embedder: Dict):
     elif "AzureOpenAITextEmbedder" in text_embedder["type"]:
         return AzureOpenAIDocumentEmbedder(**text_embedder["init_parameters"])
     elif "SentenceTransformersTextEmbedder" in text_embedder["type"]:
+        if "device" in text_embedder["init_parameters"]:
+            text_embedder["init_parameters"]["device"] = ComponentDevice.from_dict(text_embedder["init_parameters"]["device"])
+        
+        if "token" in text_embedder["init_parameters"] and text_embedder["init_parameters"]["token"]["type"] == "env_var":
+            text_embedder["init_parameters"]["token"] = Secret.from_env_var(text_embedder["init_parameters"]["token"]["env_vars"])
+        
         doc_embedder = SentenceTransformersDocumentEmbedder(**text_embedder["init_parameters"])
         doc_embedder.warm_up()
         return doc_embedder
