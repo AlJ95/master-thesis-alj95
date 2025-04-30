bdfae57d2bda39dd70f3682df5a44bcd05a73bfa
root <ke31umow@studserv.uni-leipzig.de>
2025-04-30 07:57:05 +0000

    run-eval

--- code/ragnroll_project/run-eval.sh
+++ code/ragnroll_project/run-eval.sh
@@ -1,22 +1,19 @@
-export output_path=./dense123.csv
+export config_path=./pipelines/cp0_rag_dense_4r_vllm_embedder.py
+export evaluation_data_path=./data/processed/config_val/evaluation_data.json
 export test_size=30
-export experiment_name=config-validation-ciri
-
-#export corpus_path=./data/processed/config_val/corpus
-#export evaluation_data_path=./data/processed/config_val/evaluation_data.json
-#export config_path=./configs/configuration_validation/configuration_phase/rag_hybrid_retriever.yaml
-#/srv/master-thesis-alj95/code/ragnroll_project/.venv/bin/python -m ragnroll run-evaluations $config_path $evaluation_data_path $corpus_path $output_path --test-size=$test_size --experiment-name=$experiment_name --no-baselines
-
+export experiment_name=config-validation
 
 export corpus_path=./data/processed/config_val/corpus_filtered_cleaned
+export output_path=./dense1234f13.csv
+/srv/master-thesis-alj95/code/ragnroll_project/.venv2/bin/python -m ragnroll run-evaluations $config_path $evaluation_data_path $corpus_path $output_path --test-size=$test_size --experiment-name=$experiment_name --no-baselines --positive-label=valid --negative-label=invalid
+
+
+
+
+export experiment_name=config-validation-ciri
+export output_path=./dense1234f133.csv
 export evaluation_data_path=./data/processed/config_val_ciri/evaluation_data_ciri.json
-export config_path=./configs/configuration_validation/reconfiguration_phase_2/rag_hybrid_retriever_ciri.yaml
-/srv/master-thesis-alj95/code/ragnroll_project/.venv/bin/python -m ragnroll run-evaluations $config_path $evaluation_data_path $corpus_path $output_path --test-size=$test_size --experiment-name=$experiment_name --no-baselines --positive-label=true --negative-label=false
+export config_path=./pipelines/cp0_rag_dense_4r_vllm_embedder_ciri.py
+/srv/master-thesis-alj95/code/ragnroll_project/.venv2/bin/python -m ragnroll run-evaluations $config_path $evaluation_data_path $corpus_path $output_path --test-size=$test_size --experiment-name=$experiment_name --no-baselines --positive-label=valid --negative-label=invalid
 
 
-export config_path=./configs/configuration_validation/reconfiguration_phase_2/rag_sparse_retriever_ciri.yaml
-/srv/master-thesis-alj95/code/ragnroll_project/.venv/bin/python -m ragnroll run-evaluations $config_path $evaluation_data_path $corpus_path $output_path --test-size=$test_size --experiment-name=$experiment_name --no-baselines --positive-label=true --negative-label=false 
-
-
-export config_path=./configs/configuration_validation/reconfiguration_phase_2/rag_dense_retriever_ciri.yaml
-/srv/master-thesis-alj95/code/ragnroll_project/.venv/bin/python -m ragnroll run-evaluations $config_path $evaluation_data_path $corpus_path $output_path --test-size=$test_size --experiment-name=$experiment_name --no-baselines --positive-label=true --negative-label=false 
