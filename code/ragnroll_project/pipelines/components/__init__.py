c3957fa4b885f13a201a844da4fb6db0d91c95bb
Jan Albrecht <ke31umow@studserv.uni-leipzig.de>
2025-05-06 19:03:37 +0200

    bugfix Ciri renamed for config matching

--- code/ragnroll_project/pipelines/components/__init__.py
+++ code/ragnroll_project/pipelines/components/__init__.py
@@ -1,2 +1,4 @@
 from .embedder_adapter import EmbedderAdapter
 from .ciri import CiriAnswerExtractor, CiriAnswerBuilder
+from .ciri import CiriAnswerBuilder as AnswerBuilder
+from .ciri import CiriAnswerExtractor as AnswerExtractor
